def8777f40b9fb2fea6c817f4e58aee755b57ecc
Brent <bpops@users.noreply.github.com>
2022-05-03 19:02:27 -0500

    tab widgets now generated based on sql database

--- treklist.py
+++ treklist.py
@@ -14,6 +14,7 @@ from PyQt6.QtWidgets import QWidget, QApplication, QLabel, QVBoxLayout
 from PyQt6.QtWidgets import QGroupBox, QPushButton, QHBoxLayout, QLineEdit
 from PyQt6.QtWidgets import QCheckBox, QComboBox, QSlider, QFileDialog
 from PyQt6.QtWidgets import QSizePolicy, QMenuBar, QMainWindow, QMenu, QTextBrowser
+from PyQt6.QtWidgets import QTabWidget
 from PyQt6.QtGui     import QPixmap, QIcon, QAction, QTextCursor
 from PyQt6.QtCore    import Qt, QCoreApplication#, pyqtSignal
 
@@ -33,8 +34,7 @@ except AttributeError:
    wd = os.path.dirname(os.path.realpath(__file__))
 os.chdir(wd)
 
-
-# canvas
+# window size
 init_win_width   = 1200
 init_win_height  = 600
 
@@ -42,20 +42,57 @@ class trekListApp(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.initUI()
 
-    def initUI(self):
-
+        # init UI
         self.resize(init_win_width, init_win_height)
-
         self.setWindowTitle('TrekList v0.1')
         self.show();
         self.centerWindow()
+
+        # initialize databases
+        tl_filename = "treklist.db"
+        self.tl_conn = sqlite3.connect(tl_filename)
+        self.tl_curs = self.tl_conn.cursor()
+
+        # generate tabs
+        self.tab_widget = tabWidget(self)
+        self.setCentralWidget(self.tab_widget)
+
 
     def centerWindow(self):
         qr = self.frameGeometry()
         cp = self.screen().availableGeometry().center()
         qr.moveCenter(cp)
+
+
+class tabWidget(QWidget):
+    
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+        self.layout = QVBoxLayout(self)
+
+        # pull in series info
+        parent.tl_df = pd.read_sql_query("SELECT * FROM series", parent.tl_conn)
+        abbs = parent.tl_df['abb'].tolist()
+        n_series = len(abbs)
+
+        # Initialize tab screen
+        self.tabs = QTabWidget()
+        self.tabList = []
+        for series in abbs:
+            self.tabList.append(QWidget())
+        for i in range(n_series):
+            self.tabs.addTab(self.tabList[i], abbs[i].upper())
+        
+        # Create first tab
+        #self.tab1.layout = QVBoxLayout(self)
+        #self.pushButton1 = QPushButton("PyQt5 button")
+        #self.tab1.layout.addWidget(self.pushButton1)
+        #self.tab1.setLayout(self.tab1.layout)
+        
+        # Add tabs to widget
+        self.layout.addWidget(self.tabs)
+        self.setLayout(self.layout)
 
 def main():
     app = QApplication(sys.argv)
