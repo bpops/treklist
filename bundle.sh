24c735a7a21eda5d0e0de914958b0e1349008806
Brent <bpops@users.noreply.github.com>
2022-06-06 18:38:13 -0500

    bundle script now packages a disk image for installation

--- bundle.sh
+++ bundle.sh
@@ -9,5 +9,20 @@ pyinstaller treklist.spec
 iconutil -c icns -o imgs/icon-windowed.icns imgs/icon.iconset
 mv imgs/icon-windowed.icns dist/treklist.app/Contents/Resources/.
 
-# rename app
-mv dist/treklist.app dist/TrekList.app
+# prep dist folder
+mkdir -p dist/dmg
+rm -r dist/dmg/*
+cp -r dist/TrekList.app dist/dmg/TrekList.app
+test -f "dist/Treklist.dmg" && rm "dist/Treklist.dmg"
+
+# create the disk image
+create-dmg \
+    --volname "TrekList" \
+    --window-pos 200 120 \
+    --window-size 600 300 \
+    --icon-size 100 \
+    --icon "TrekList.app" 175 120 \
+    --hide-extension "TrekList.app" \
+    --app-drop-link 425 120 \
+    "dist/TrekList.dmg" \
+    "dist/dmg/"
